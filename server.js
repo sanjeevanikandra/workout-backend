8a55b3c523d45ae004116e424913a285be8b213a
sanjeevani_kandra_09 <sanjeevanikandra24@navgurukul.org>
2024-10-07 22:08:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/sessions', sessionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
